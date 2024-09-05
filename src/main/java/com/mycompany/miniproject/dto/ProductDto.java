8b30b8bdb9380033bd238de027671a280015a690
junbum6694 <wnsqja6694@gmail.com>
2024-09-05 18:58:12 +0900

    Add: ProductDto 추가

---
+++ src/main/java/com/mycompany/miniproject/dto/ProductDto.java
@@ -0,0 +1,17 @@
+package com.mycompany.miniproject.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ProductDto {
+	private int productId;
+	private String productName;
+	private int productPrice;
+	private int productStock;
+	private String productSummary;
+	private String productDetailSummary;
+	private Date productRegDate;
+	private String category;
+}
