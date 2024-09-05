330e41de2b664cf03f8a9e4b8cdf6adc652392f6
Yerin_Hwang <babybbmm59@gmail.com>
2024-09-05 20:10:43 +0900

    fix: CartDto 재업로드

--- src/main/java/com/mycompany/miniproject/dto/CartDto.java
+++ src/main/java/com/mycompany/miniproject/dto/CartDto.java
@@ -3,7 +3,7 @@ package com.mycompany.miniproject.dto;
 import lombok.Data;
 
 @Data
-public class Cart {
+public class CartDto {
 	private String userId;
 	private int productId;
 	private int productQty;
