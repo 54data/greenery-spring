85babfeda7bc760c2effef615d0cd06853e5a719
Yerin_Hwang <babybbmm59@gmail.com>
2024-09-11 11:53:19 +0900

    Fix : detailPage 하단 이미지 나오도록 수정2차

--- src/main/java/com/mycompany/miniproject/controller/ProductController.java
+++ src/main/java/com/mycompany/miniproject/controller/ProductController.java
@@ -1,12 +1,10 @@
 package com.mycompany.miniproject.controller;
 
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -42,10 +40,18 @@ public class ProductController {
 	@RequestMapping("/detailpage")
 	public String detailpage(@RequestParam int productId, Model model) throws Exception  {
 	    ProductDto product = productService.getProductDetail(productId);  
-	    ProductImageDto productImage = productService.getMainImg(productId);
+	    List<ProductImageDto> productImages = productService.getProductImgs(productId);
+	    
+	    List<String> imageUrls = new ArrayList<>();
+	    
+	    
+	    for(ProductImageDto productImage : productImages) {
+	    	String imageUrl = "loadImg?productId=" + productId + "&product_img_usage=" + productImage.getProductImgUsage();
+	    	imageUrls.add(imageUrl);
+	    }
 	    
 	    model.addAttribute("product", product);
-	    model.addAttribute("productImage", productImage);
+	    model.addAttribute("imageUrls", imageUrls);
 	    
 	    return "product/detailpage"; 
 	}
@@ -56,25 +62,32 @@ public class ProductController {
 		return "product/search";
 	}
 	
-//	@GetMapping("loadImg")
-//	public void loadImg(int productId, HttpServletResponse response) throws Exception {
-//		
-//		List<ProductImageDto> productImages = productService.getProductImgs(productId);
-//		
-//		for (ProductImageDto productImage : productImages) {
-//	        String contentType = productImage.getProductImgType();
-//	        response.setContentType(contentType);
-//	        
-//	        String fileName = productImage.getProductImgName();
-//	        String encodingFileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
-//	        response.setHeader("Content-Disposition", "inline; filename=\"" + encodingFileName + "\"");
-//
-//	        OutputStream out = response.getOutputStream();
-//	        out.write(productImage.getProductImg());
-//	        out.flush();
-//	        out.close();
-//	    }
-//	}
+	@GetMapping("loadImg")
+	public void loadImg(int productId, String product_img_usage, HttpServletResponse response) throws Exception {
+		
+		List<ProductImageDto> productImages = productService.getProductImgs(productId);
+		
+		
+		for (ProductImageDto productImage : productImages) {
+			String fileUsage = productImage.getProductImgUsage();
+				        
+	        if(!fileUsage.equals(product_img_usage)) {
+	        	String contentType = productImage.getProductImgType();
+		        response.setContentType(contentType);
+	        		       
+	        	
+	        	String fileName = productImage.getProductImgName();
+		        String encodingFileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+		        response.setHeader("Content-Disposition", "inline; filename=\"" + encodingFileName + "\"");
+
+		        OutputStream out = response.getOutputStream();
+		        out.write(productImage.getProductImg());
+		        out.flush();
+		        out.close();
+	        }
+	        
+	    }
+	}
 
 	@GetMapping("loadMainImg")
 	public void loadMainImg(int productId, HttpServletResponse response) throws Exception {
--- src/main/webapp/WEB-INF/views/product/detailpage.jsp
+++ src/main/webapp/WEB-INF/views/product/detailpage.jsp
@@ -25,9 +25,14 @@
 
 			<!-- 이미지 사이드 쇼 -->
 			<div class="slideshow-container">
-
+				<c:forEach var="imageUrl" items="${imageUrls}">
+			        <div class="mySlides fade">
+			            <img src="${imageUrl}">
+			        </div>
+			    </c:forEach>
+			    
 				<!-- Full-width images with number and caption text -->
-			<div class="mySlides fade">
+<%--			<div class="mySlides fade">
 					<img
 						src="loadMainImg?productId=${product.productId}"
 						alt="Slide 1">
@@ -45,11 +50,8 @@
 						alt="Slide 3">
 				</div>
 				
-<%-- 					<c:forEach var="imageUrl" items="${imageUrls}">
-				        <div class="mySlides fade">
-				            <img src="loadMainImg?productId=${product.productId}">
-				        </div>
-				    </c:forEach> --%>
+ --%>
+
 			</div> 
 			
 
@@ -94,7 +96,7 @@
 		</a>
 
 		<div class="currentSlide-container">
-			<span class="dot" onclick="currentSlide(1)"> <img
+<%-- 		<span class="dot" onclick="currentSlide(1)"> <img
 				src="loadMainImg?productId=${product.productId}"
 				alt="currentSlide(1)" style="width: 25%">
 			</span> <span class="dot" onclick="currentSlide(2)"> <img
@@ -103,35 +105,17 @@
 			</span> <span class="dot" onclick="currentSlide(3)"> <img
 				src="loadMainImg?productId=${product.productId}"
 				alt="currentSlide(3)" style="width: 25%">
-			</span>
+			</span> --%>
 			
-			
-<%-- 			<c:forEach var="imageUrl" items="${imageUrls}" varStatus="status">
+			<c:forEach var="imageUrl" items="${imageUrls}" varStatus="status">
 		        <span class="dot" onclick="currentSlide(${status.index + 1})">
-		            <img src="${imageUrl}" alt="Slide ${status.index + 1}" style="width: 25%">
+		            <img src="${imageUrl}" style="width: 25%">
 		        </span>
-    		</c:forEach> --%>
-		</div> 
-
-
-
-
-<!-- 		<div class="currentSlide-container">
-			<span class="dot" onclick="currentSlide(1)"> <img
-				src="https://i.pinimg.com/564x/db/a1/f2/dba1f28adc32bc960e89ee4dd4ffa027.jpg"
-				alt="currentSlide(1)" style="width: 25%">
-			</span> <span class="dot" onclick="currentSlide(2)"> <img
-				src="https://i.pinimg.com/564x/d9/d3/82/d9d3829fc798b6107ca26649c3000b13.jpg"
-				alt="currentSlide(2)" style="width: 25%">
-			</span> <span class="dot" onclick="currentSlide(3)"> <img
-				src="https://i.pinimg.com/564x/17/71/74/177174a71afb907c30f5af5563b7375d.jpg"
-				alt="currentSlide(3)" style="width: 25%">
-			</span>
-		</div> -->
-
+		    </c:forEach>
 		<a class="next" onclick="plusSlides(1)"> <img
 			src="${pageContext.request.contextPath}/resources/image/right-icon.png" alt="Next" style="width: 80%">
 		</a>
+		</div>
 	</div>
 
 	<!-- 상세페이지, 리뷰페이지 -->
