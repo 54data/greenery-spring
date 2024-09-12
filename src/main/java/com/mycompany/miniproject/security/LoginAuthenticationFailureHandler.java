640913246444efd898b3a3ec49a557b19ac2eb78
54data <ehdvkf02@naver.com>
2024-09-12 14:50:17 +0900

    Add: 로그인 인증 관련 핸들러 생성

---
+++ src/main/java/com/mycompany/miniproject/security/LoginAuthenticationFailureHandler.java
@@ -0,0 +1,19 @@
+package com.mycompany.miniproject.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+public class LoginAuthenticationFailureHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
+}
