bb2285cedd48dbf26f6c9bb77c9085a795760ae0
54data <ehdvkf02@naver.com>
2024-09-13 02:59:18 +0900

    Fix: 로그인 성공 및 실패 핸들러 상속 클래스 오류 수정

--- src/main/java/com/mycompany/miniproject/security/LoginAuthenticationSuccessHandler.java
+++ src/main/java/com/mycompany/miniproject/security/LoginAuthenticationSuccessHandler.java
@@ -6,14 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
-public class LoginAuthenticationSuccessHandler extends SimpleUrlAuthenticationFailureHandler {
+public class LoginAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 	@Override
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException exception) throws IOException, ServletException {
-		setDefaultFailureUrl("/account/loginForm");
-		super.onAuthenticationFailure(request, response, exception);
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws ServletException, IOException {
+		super.onAuthenticationSuccess(request, response, authentication);
 	}
 }
