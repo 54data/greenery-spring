f767aa6dcd6d2d1101bad9af8756eb88b3fcf4bf
54data <ehdvkf02@naver.com>
2024-09-26 17:36:07 +0900

    Fix: 장바구니 요청시 이미 담긴 상품일 때 alert창 멘트 수정

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -243,7 +243,7 @@ $('.cart-icon').on('click', function(event){
 			if(response == "exist"){
 				console.log("exist 왔음");
 				Swal.fire({
-					html : "장바구니에 상품이 담겼습니다.<br>장바구니로 이동하시겠습니까?",
+					html : "이미 담긴 상품입니다.<br>장바구니로 이동하시겠습니까?",
 					cancelButtonText : "쇼핑 계속하기",
 					confirmButtonText : "장바구니 확인",
 					showCancelButton : true,
