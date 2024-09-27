afd724a9f206c43db172ca79046d1aa343f23227
junbum6694 <wnsqja6694@gmail.com>
2024-09-27 16:31:42 +0900

    Remove: 불필요 코드 삭제

--- src/main/webapp/resources/js/notices.js
+++ src/main/webapp/resources/js/notices.js
@@ -1,27 +1,3 @@
-//$.ajax({         
-//	type: "get",
-//	url: "../../content/notices.json",
-//	dataType: "json",
-//	success: function(data) {
-//		console.log("통신성공");
-//		let notices = data.notices;
-//		let content = $('.content ');
-//		notices.forEach(notices => {
-//			let noticeItem = `<div class="notices-item">
-//			<div class="noticeId">${notices.noticeId}</div>
-//			<div class="title">${notices.title}</div>
-//			<div class="registrationDate">${notices.registrationDate}</div>
-//			</div>`;
-//			content.append(noticeItem);
-//			content.append(`<div class="divider"></div>`);
-//		});
-//		
-//	},
-//	error: function() {
-//		console.log("통신에러");
-//	}       	  	
-//})
-
 $(document).ready(function () {
 	$(".title").click(function() {
 		location.href="noticeContent";
